9314678c2b38c214da8b34aaac61a73b04a4ddb8
Suphawadee <ked.suphawadee@gmail.com>
2021-03-08 11:27:52 +0700

    change next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   distDir: 'build',
-  basePath: '/next-contentful/out/',
+  basePath: '/next-contentful/out',
   assetPrefix: '/next-contentful/out/',
   // assetPrefix: !debug ? '/next-contentful/out/' : '',
   env: {
