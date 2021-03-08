df0bbe0bcc8ace18b79e2ea12d3c238cbbbd91eb
Suphawadee <ked.suphawadee@gmail.com>
2021-03-08 11:59:35 +0700

    update build

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Index({ posts, category }) {
           {/* you can delete this component or you can use this for your page header. */}
           <ul className="nav justify-content-center">
             <li className="nav-item">
-              <a className="nav-link" href="#">All</a>
+              <a className="nav-link" href={`/category`}>All</a>
             </li>
           {
             category?.map(({fields}, index) => (
