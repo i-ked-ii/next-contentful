462834bff13b770a25d314760e7da3b689b094ba
Narueta_XD <annoi@live.com>
2021-03-09 08:43:30 +0700

    add docs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
+const path = require('path')
+const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
+  distDir: 'build',
+  // basePath: '/next-contentful/out',
+  assetPrefix: !debug ? '/next-contentful/docs' : '',
+  // assetPrefix: '/next-contentful/out/',
   env: {
-    REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-    REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+    REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID || process.env.CONTENTFUL_SPACE_ID,
+    REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_ACCESS_TOKEN,
   },
-  assetPrefix: 'https://twinsynergy.github.io/next-contentful'
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  // reactStrictMode: true,
+  trailingSlash: true,
 };
