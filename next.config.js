fa9f86a7d0be0b9cba6ff2a7f0a626ecd5a5a4d2
Narueta_XD <annoi@live.com>
2021-03-10 13:35:16 +0700

    add to page

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ const path = require('path')
 const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
   distDir: 'build',
-  basePath: '/next-contentful',
-  assetPrefix: '/next-contentful/',
+  // basePath: '/next-contentful',
+  // assetPrefix: '/next-contentful/',
   // assetPrefix: '/next-contentful/out/',
   env: {
     REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID || process.env.CONTENTFUL_SPACE_ID,
     REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_ACCESS_TOKEN,
-    REACT_APP_BASE_PATH: '/next-contentful'
+    REACT_APP_BASE_PATH: ''
   },
   exportPathMap: async function (
     defaultPathMap,
